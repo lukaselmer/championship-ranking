7e04f07be2682320bd99733218333359da6c6172
Lukas Elmer <lukas.elmer@gmail.com>
2025-06-03 21:40:35 +0200

    Add example ranking data

--- src/championshipRanking/rankingData.ts
+++ src/championshipRanking/rankingData.ts
@@ -1,2 +1,53 @@
+// from https://speedhive.mylaps.com/organizations/95657/championships/138156
+
 export const rankingData = `
+1	25	Swen Graf	56	53	65	102	34	77	117	51	92
+2	6	Noa Glunk	110	124	112	x	x	124	x	124	x
+3	244	Luca Jenni	90	110	110	99	117	55	x	x	x
+4	405	Jan Walser	20	71	59	110	82	28	117	71	0
+5	183	Joel Herren	35	45	68	87	x	84	65	32	87
+6	712	Jérôme Schneider	52	90	112	x	x	105	x	110	x
+7	407	Thomas Bachmann	40	0	90	x	100	60	x	53	112
+8	799	Noah Rohner	27	47	62	0	62	70	71	27	65
+9	250	Pascal Bischof	40	77	43	x	x	65	x	65	74
+10	169	Bruno Peixoto	17	x	28	56	44	39	87	29	59
+11	46	Patrick Hasenfratz	27	x	38	x	82	x	85	41	59
+12	162	Marc Huber	x	x	x	x	x	59	90	56	80
+13	68	Michael Sprenger	62	79	77	x	x	32	x	20	x
+14	18	Andreas Jenni	x	x	39	x	x	47	x	49	107
+15	100	Pascal Huber	74	x	x	x	x	71	x	76	x
+16	141	Lukas Indergand	x	35	28	x	50	34	45	28	x
+17	585	Manuel Goncalves	27	38	33	x	80	26	0	x	x
+18	87	Alessandro Panzeri	17	x	x	34	30	x	59	15	42
+19	32	Sascha Sohm	24	50	x	0	x	30	45	35	x
+20	987	Benedikt Pieper	62	x	42	x	62	x	x	x	x
+21	135	Simon Birrer	47	x	23	x	x	x	x	95	x
+22	211	Jonas Bötsch	x	x	x	76	33	27	x	21	x
+23	198	Joël Morgenthaler	13	x	x	73	27	x	x	21	x
+24	69	Manuel Vassella	24	53	x	x	x	x	x	56	x
+25	11	Eric Rütsche	68	x	x	x	x	x	x	59	x
+26	97	Yves Stadelmann	124	x	x	x	x	x	x	x	x
+27	411	Adrian Leutenegger	x	x	x	x	117	x	x	x	x
+28	12	Diego Papillon	x	x	x	84	x	x	x	31	x
+29	38	Dominik Dürrenmatt	35	x	x	x	x	x	x	x	79
+30	36	Samuel Lüthi	x	x	10	62	19	x	x	6	x
+31	93	Randy Götzl	x	95	x	x	x	x	x	x	x
+32	931	Yannick Bärtschi	95	x	x	x	x	x	x	x	x
+33	28	Manuel Frischknecht	x	x	x	28	21	x	x	8	36
+34	24	Jan Ramsauer	x	x	x	x	40	32	x	18	x
+35	62	Denis Weber	x	x	x	25	x	x	53	11	x
+36	14	David Sousa	x	x	x	42	24	x	x	4	x
+37	313	Remy Schelbert	23	x	x	x	x	x	x	x	42
+38	95	David De Sousa	x	x	29	0	32	x	x	x	x
+39	7	Sven Öhri	x	33	x	0	x	x	x	14	x
+40	298	Marcelo Da Costa	x	x	x	x	44	x	x	x	0
+41	84	Ruedi Keller	x	x	x	x	x	43	x	x	x
+42	17	Steve Baggenstos	x	x	x	18	23	x	x	2	x
+43	21	Carlos Oliveira	x	x	39	x	x	x	x	x	x
+44	256	Sven Jans	x	x	x	x	35	x	x	x	x
+45	35	Thomas Keller	x	x	33	x	x	x	x	x	x
+46	74	Martin Köhler	x	x	x	x	x	x	22	x	x
+47	91	Danny Schatt	6	x	x	x	x	x	x	12	x
+48	301	Jan Kupfer	x	x	x	0	10	x	x	x	x
+49	351	Peter Küng	x	x	x	x	x	x	x	0	x
 `
